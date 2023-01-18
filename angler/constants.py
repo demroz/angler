44604946ea19fbcb274cd93129a83683224c11be
demroz <mzhelyez@gmail.com>
2023-01-18 13:37:35 -0800

    changed constants in default package to be equal to 1.0

--- angler/constants.py
+++ angler/constants.py
@@ -1,7 +1,7 @@
 from numpy import sqrt
 
-EPSILON_0 = 8.85418782e-12
-MU_0 = 1.25663706e-6
+EPSILON_0 = 1.0 #8.85418782e-12 # set everything to 1.0
+MU_0 = 1.0 #1.25663706e-6 # set everything to 1.0
 C_0 = sqrt(1/EPSILON_0/MU_0)
 ETA_0 = sqrt(MU_0/EPSILON_0)
 
@@ -13,7 +13,7 @@ try:
 except:
     DEFAULT_SOLVER = 'scipy'
 
-DEFAULT_LENGTH_SCALE = 1e-6  # microns
+DEFAULT_LENGTH_SCALE = 1.0 # set everything to 1.0
 
 def n2_from_chi3(chi3):
     # takes chi3 in units of m^2 / V^2 and gives n2
